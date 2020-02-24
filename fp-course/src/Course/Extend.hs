2f94f73760c48189c0620b5d9408c302a0cb33fc
Tsimon Dorakh <tsimon.dorakh@accelya.com>
2020-02-24 20:15:18 +0300

    [fp-course] Extend and Comonad

--- fp-course/src/Course/Extend.hs
+++ fp-course/src/Course/Extend.hs
@@ -33,8 +33,8 @@ instance Extend ExactlyOne where
     (ExactlyOne a -> b)
     -> ExactlyOne a
     -> ExactlyOne b
-  (<<=) =
-    error "todo: Course.Extend (<<=)#instance ExactlyOne"
+  (<<=) f =
+    ExactlyOne . f
 
 -- | Implement the @Extend@ instance for @List@.
 --
@@ -51,8 +51,8 @@ instance Extend List where
     (List a -> b)
     -> List a
     -> List b
-  (<<=) =
-    error "todo: Course.Extend (<<=)#instance List"
+  (<<=) _ Nil = Nil
+  (<<=) f x@(_:.t) = f x :. (<<=) f t
 
 -- | Implement the @Extend@ instance for @Optional@.
 --
@@ -66,8 +66,7 @@ instance Extend Optional where
     (Optional a -> b)
     -> Optional a
     -> Optional b
-  (<<=) =
-    error "todo: Course.Extend (<<=)#instance Optional"
+  (<<=) f a = f . Full <$> a
 
 -- | Duplicate the functor using extension.
 --
@@ -86,5 +85,5 @@ cojoin ::
   Extend k =>
   k a
   -> k (k a)
-cojoin =
-  error "todo: Course.Extend#cojoin"
+cojoin = (<<=) id
+
