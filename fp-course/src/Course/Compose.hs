523a36631dd4f83daae826620d5f7d53569d11fe
Tsimon Dorakh <tsimon.dorakh@accelya.com>
2020-02-24 08:46:27 +0300

    [fp-course] Init

---
+++ fp-course/src/Course/Compose.hs
@@ -0,0 +1,57 @@
+{-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE InstanceSigs #-}
+
+module Course.Compose where
+
+import Course.Core
+import Course.Functor
+import Course.Applicative
+import Course.Monad
+import Course.Contravariant
+
+-- Exactly one of these exercises will not be possible to achieve. Determine which.
+
+newtype Compose f g a =
+  Compose (f (g a)) deriving (Show, Eq)
+
+-- Implement a Functor instance for Compose
+instance (Functor f, Functor g) =>
+    Functor (Compose f g) where
+  (<$>) ::
+    (a -> b) ->
+    Compose f g a ->
+    Compose f g b
+  (<$>) f (Compose fga) =
+    Compose ((f <$>) <$> fga)
+
+instance (Applicative f, Applicative g) =>
+  Applicative (Compose f g) where
+-- Implement the pure function for an Applicative instance for Compose
+  pure :: a -> Compose f g a
+  pure =
+    Compose . pure . pure
+
+-- Implement the (<*>) function for an Applicative instance for Compose
+  (<*>) ::
+    Compose f g (a -> b) ->
+    Compose f g a ->
+    Compose f g b
+  (<*>) (Compose fgab) (Compose fga) =
+    Compose (lift2 (<*>) fgab fga)
+--     Compose (((<*>) <$> fgab) <*> fga)
+
+instance (Monad f, Monad g) =>
+  Monad (Compose f g) where
+-- Implement the (=<<) function for a Monad instance for Compose
+  (=<<) =
+    error "todo: Course.Compose (=<<)#instance (Compose f g)"
+
+-- Note that the inner g is Contravariant but the outer f is
+-- Functor. We would not be able to write an instance if both were
+-- Contravariant; why not?
+instance (Functor f, Contravariant g) =>
+  Contravariant (Compose f g) where
+-- Implement the (>$<) function for a Contravariant instance for Compose
+  (>$<) =
+    error "todo: Course.Compose (>$<)#instance (Compose f g)"
---
+++ fp-course/src/Course/Traversable.hs
@@ -0,0 +1,138 @@
+{-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE InstanceSigs #-}
+
+module Course.Traversable where
+
+import Course.Core
+import Course.Functor
+import Course.Applicative
+import Course.List
+import Course.ExactlyOne
+import Course.Optional
+import Course.Compose
+
+-- | All instances of the `Traversable` type-class must satisfy three laws. These
+-- laws are not checked by the compiler. These laws are given as:
+--
+-- * The law of naturality
+--   `∀f g. f . traverse g ≅ traverse (f . g)`
+--
+-- * The law of identity
+--   `∀x. traverse ExactlyOne x ≅ ExactlyOne x`
+--
+-- * The law of composition
+--   `∀f g. traverse ((g <$>) . f) ≅ (traverse g <$>) . traverse f`
+class Functor t => Traversable t where
+  traverse ::
+    Applicative k =>
+    (a -> k b)
+    -> t a
+    -> k (t b)
+
+instance Traversable List where
+  traverse ::
+    Applicative k =>
+    (a -> k b)
+    -> List a
+    -> k (List b)
+  traverse f =
+    foldRight (\a b -> (:.) <$> f a <*> b) (pure Nil)
+
+-- traverse (\a -> Full (a * 2)) (ExactlyOne 3)
+instance Traversable ExactlyOne where
+  traverse ::
+    Applicative k =>
+    (a -> k b)
+    -> ExactlyOne a
+    -> k (ExactlyOne b)
+  traverse f (ExactlyOne a) =
+    ExactlyOne <$> f a
+
+instance Traversable Optional where
+  traverse ::
+    Applicative k =>
+    (a -> k b)
+    -> Optional a
+    -> k (Optional b)
+  traverse f =
+    optional (\a -> pure <$> f a) (pure Empty)
+
+-- | Sequences a traversable value of structures to a structure of a traversable value.
+--
+-- >>> sequenceA (ExactlyOne 7 :. ExactlyOne 8 :. ExactlyOne 9 :. Nil)
+-- ExactlyOne [7,8,9]
+--
+-- >>> sequenceA (Full (ExactlyOne 7))
+-- ExactlyOne (Full 7)
+--
+-- >>> sequenceA (Full (*10)) 6
+-- Full 60
+sequenceA ::
+  (Applicative k, Traversable t) =>
+  t (k a)
+  -> k (t a)
+sequenceA = traverse id
+
+
+instance (Traversable f, Traversable g) =>
+  Traversable (Compose f g) where
+-- Implement the traverse function for a Traversable instance for Compose
+  traverse ::
+    Applicative k =>
+      (a -> k b) ->
+      Compose f g a ->
+      k (Compose f g b)
+  traverse f (Compose x) =
+    Compose <$> traverse (traverse f) x
+
+
+-- | The `Product` data type contains one value from each of the two type constructors.
+data Product f g a =
+  Product (f a) (g a) deriving (Show, Eq)
+
+instance (Functor f, Functor g) =>
+  Functor (Product f g) where
+-- Implement the (<$>) function for a Functor instance for Product
+  (<$>) :: (a -> b) -> Product f g a -> Product f g b
+  (<$>) f (Product t1 t2) =
+    Product (f <$> t1) (f <$> t2)
+
+instance (Traversable f, Traversable g) =>
+  Traversable (Product f g) where
+-- Implement the traverse function for a Traversable instance for Product
+  traverse ::
+    Applicative k =>
+      (a -> k b) ->
+      Product f g a ->
+      k (Product f g b)
+  traverse f (Product t1 t2) =
+    lift2 Product (traverse f t1) (traverse f t2)
+--  Product <$> traverse f a <*> traverse f b
+
+-- | The `Coproduct` data type contains one value from either of the two type constructors.
+data Coproduct f g a =
+  InL (f a)
+  | InR (g a) deriving (Show, Eq)
+
+instance (Functor f, Functor g) =>
+  Functor (Coproduct f g) where
+-- Implement the (<$>) function for a Functor instance for Coproduct
+  (<$>) ::
+    (a -> b) ->
+    Coproduct f g a ->
+    Coproduct f g b
+  (<$>) f (InL fa) = InL (f <$> fa)
+  (<$>) f (InR ga) = InR (f <$> ga)
+
+instance (Traversable f, Traversable g) =>
+  Traversable (Coproduct f g) where
+-- Implement the traverse function for a Traversable instance for Coproduct
+  traverse :: Applicative k =>
+    (a -> k b) ->
+    Coproduct f g a ->
+    k (Coproduct f g b)
+  traverse f (InL fa) =
+    InL <$> traverse f fa
+  traverse f (InR ga) =
+    InR <$> traverse f ga
