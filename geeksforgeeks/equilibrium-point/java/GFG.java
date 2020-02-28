6fbca822905372e8ce985806cf87ff63e55438c2
Tsimon Dorakh <tsimon.dorakh@accelya.com>
2020-02-28 19:40:35 +0300

    [geeksforgeeks] Equilibrium point https://practice.geeksforgeeks.org/problems/equilibrium-point/0

---
+++ geeksforgeeks/equilibrium-point/java/GFG.java
@@ -0,0 +1,146 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+/**
+ * Equilibrium point
+ * https://practice.geeksforgeeks.org/problems/equilibrium-point/0
+ */
+public class GFG {
+    private static int solve(int[] xs) {
+        int lsum = 0;
+        for (int x : xs) {
+            lsum += x;
+        }
+
+        int rsum = 0;
+        for (int i = xs.length - 1; i >= 0; i--) {
+            if (rsum == lsum - xs[i]) return i + 1;
+            rsum += xs[i];
+            lsum -= xs[i];
+        }
+
+        return -1;
+    }
+
+    public static void main(String[] args) {
+//        int[][] xss = readInput(new FastReader("input-00-0.txt"));
+        int[][] xss = readInput(new FastReader(System.in));
+
+//        print(xss);
+
+        for (int[] xs : xss) {
+            print(solve(xs));
+        }
+    }
+
+    private static void println(int x) {
+        System.out.println(x);
+    }
+
+    private static void println(long x) {
+        System.out.println(x);
+    }
+
+    private static void print(Collection xs) {
+        StringBuilder stringBuilder = new StringBuilder(xs.size() * 2);
+
+        xs.forEach(x -> stringBuilder.append(x).append(" "));
+
+        System.out.println(stringBuilder.toString());
+    }
+
+    private static void print(int x) {
+        System.out.println(x);
+    }
+
+    private static void print(int[] xs) {
+        for (int x : xs) {
+            System.out.print(x + " ");
+        }
+        System.out.println();
+    }
+
+    private static void print(int[][] xss) {
+        for (int[] xs : xss) {
+            print(xs);
+        }
+    }
+
+    static int[][] readInput(FastReader fastReader) {
+        int[][] xss = new int[fastReader.nextInt()][];
+
+        for (int i = 0; i < xss.length; i++) {
+            xss[i] = new int[fastReader.nextInt()];
+
+            for (int j = 0; j < xss[i].length; j++) {
+                xss[i][j] = fastReader.nextInt();
+            }
+        }
+
+        return xss;
+    }
+
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new
+                    InputStreamReader(System.in));
+        }
+
+        public FastReader(InputStream inputStream) {
+            br = new BufferedReader(new
+                    InputStreamReader(inputStream));
+        }
+
+        public FastReader(String filename) {
+            br = new BufferedReader(new
+                    InputStreamReader(GFG.class.getResourceAsStream(filename)));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            return str;
+        }
+    }
+
+}
